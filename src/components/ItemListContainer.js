5177576f830da7bc50b8de8f49398eabdb742532
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-10-19 11:47:23 -0300

    ItemList imported in ItemListContainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,13 @@
 import { Col, Row} from "react-bootstrap";
 import "./ItemListContainer.css";
+import ItemList from "./ItemList.js"
 
 const ItemListContainer = ({greeting}) => {
     return <div className="content">
             <Row>
                 <Col>
                     <h1>{greeting}</h1>
+                    <ItemList />
                 </Col>
             </Row>
         </div>
