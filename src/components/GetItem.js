9c3540e5ee3a4e109c509f5d0a847344f6a38674
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-10-24 10:42:02 -0300

    prueba

--- src/components/GetItem.js
+++ src/components/GetItem.js
@@ -1,57 +1,30 @@
-import React from "react";
-import { products } from "../products";
 
-export default GetItems;
-
-// export const getItem = (
-//     products,
-//     setMessage,
-//     setIsSuccess,
-//     setIsLoading,
-//     setIsFinished,
-//     setCurrentProducts
-//   ) => {
-//     const productsResponse = new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(products);
-//       }, 2000);
-//     });
-  
-//     productsResponse
-//       .then((result) => {
-//         setIsSuccess(true);
-//         setCurrentProducts(result);
-//       })
-//       .catch((error) => {
-//         console.log(`Error in progress ${error}`);
-//         setMessage(`Error: ${error}`);
-//         setIsSuccess(false);
-//       })
-//       .finally(() => {
-//         setIsFinished(true);
-//         setIsLoading(false);
-//       });
-//   };
-
-
-
-
-const GetItems = ({products}) => {
-    const productsResponse = new Promise((resolve, reject, array) => {
-        setTimeout(function(){
-            resolve(products);
-        }, 3000);
+export const getItem = (
+    products,
+    setMessage,
+    setIsSuccess,
+    setIsLoading,
+    setIsFinished,
+    setCurrentProducts
+  ) => {
+    const productsResponse = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(products);
+      }, 2000);
     });
-
-    productsResponse.then((result) => {
-        console.log(result);
-    })
-
-
-    // productsResponse.then(array => {
-    //     console.log(array.find(product => product.id === 3));
-    // })
-
-    return (products.find(product => product.id === 3));
-}
-
+  
+    productsResponse
+      .then((result) => {
+        setIsSuccess(true);
+        setCurrentProducts(result.find(id => id.id === 3));
+      })
+      .catch((error) => {
+        console.log(`Error in progress ${error}`);
+        setMessage(`Error: ${error}`);
+        setIsSuccess(false);
+      })
+      .finally(() => {
+        setIsFinished(true);
+        setIsLoading(false);
+      });
+  };
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,37 +1,83 @@
-import { Container, Spinner } from "react-bootstrap";
-import "./ItemDetailContainer.css";
-import ItemDetail from "./ItemDetail";
-import { products } from "../products";
+// import { Container, Spinner } from "react-bootstrap";
+// import "./ItemDetailContainer.css";
+// import ItemDetail from "./ItemDetail";
+// import { products } from "../products";
 
 
-const getItem = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(products);
-  }, 2000);
-});
+// const getItem = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(products);
+//   }, 2000);
+// });
 
-getItem
-  .then((result) => {
-    const item = result.find(id => id.id === 3)
-    console.log(item);
-    return item;
-  })
+// getItem
+//   .then((result) => {
+//     const item = result.find(id => id.id === 3)
+//     console.log(item);
+//     return item;
+//   })
 
 
 
 
   
 
-const ItemDetailContainer = () => {
-  const item = getItem
-  console.log(item);
+// const ItemDetailContainer = () => {
+//   const item = getItem
+//   console.log(item);
+//   return (
+//     <Container fluid id="detailContainer">
+//       <ItemDetail item= {item} {...item} />
+//     </Container>
+
+
+//   )}
+
+
+// export default ItemDetailContainer;
+
+
+import { useEffect, useState } from "react";
+import { Container, Spinner } from "react-bootstrap";
+import "./ItemDetailContainer.css";
+import { getItem } from "./GetItem";
+import ItemDetail from "./ItemDetail";
+
+
+const ItemDetailContainer = ({ products }) => {
+  const [message, setMessage] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFinished, setIsFinished] = useState(false);
+  const [currentProducts, setCurrentProducts] = useState([]);
+
+  useEffect(() => {
+    if (products) {
+      getItem(
+        products,
+        setMessage,
+        setIsSuccess,
+        setIsLoading,
+        setIsFinished,
+        setCurrentProducts
+      );
+    }
+  }, products);
+  console.log(products);
+  
+
   return (
-    <Container fluid id="detailContainer">
-      <ItemDetail item= {item} {...item} />
-    </Container>
-
-
-  )}
-
+    <div className="container-fluid" id="listContainer">
+      <h3 className={isSuccess ? "successMessage" : "errorMessages"}>
+        {message}
+      </h3>
+      {isLoading && <Container fluid> <Spinner animation="border" variant="secondary"/></Container>}
+      {isFinished}
+      {currentProducts.map((item) => (
+        <ItemDetail item= {item} {...item} />
+      ))}
+    </div>
+  );
+};
 
 export default ItemDetailContainer;
