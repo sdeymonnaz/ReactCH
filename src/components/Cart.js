2137f73dcc472eb12488566fcd13beb52c1858d3
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-11-02 21:13:17 -0300

    cart layout fixed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,9 @@
 import React, {useContext} from "react";
-import ItemDetail from "./ItemDetail";
+import {Table} from "react-bootstrap";
+//import ItemDetail from "./ItemDetail";
 import './Cart.css';
 import { CartContext } from "../context/cartContext";
+
 
 
 
@@ -10,12 +12,31 @@ const Cart = () => {
   const { items } = useContext(CartContext);
   console.log('items en cart', items);
   return (
-    <div id="cartContainer">
+    <div id="cartContainer" className="p-3">
       <h1>Cart</h1>
       {items.map((currentItem) => {
         const { item } = currentItem;
-        console.log(item);
-        return <ItemDetail {...item} quantity={currentItem.quantity} />
+        const quantity = currentItem.quantity;
+        
+        return (
+          <>
+          <div className="container">
+            <Table striped bordered hover variant="dark" id="cartTable">
+              <tbody>
+                <tr style={{disply:'flex', justifyContent:'start'}}>
+                  <td style={{width: "4rem"}}>ID: {item.id}</td>
+                  <td style={{width: "10rem"}}><img alt="cover" src={item.pictureUrl} style={{width: "60px"}}></img></td>
+                  <td style={{width: "26rem", textAlign:'left'}}>{item.title}</td>
+                  <td style={{width: "10rem", textAlign:'left'}}>${item.price.toFixed(2)}</td>
+                  <td>Quantity: {quantity}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          </>
+        
+        //<ItemDetail {...item} quantity={currentItem.quantity} />
+        )
       })}
     </div>
   );
