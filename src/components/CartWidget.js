c55e505d22b33831b9ad7f1bc0e99b4733425587
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-11-12 10:05:31 -0300

    cartWidget fixed

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -8,19 +8,18 @@ import { FaShoppingCart } from 'react-icons/fa';
 const CartWidget = () => {
     const { items } = useContext(CartContext);
     const {countItemsInCart} = useContext(CartContext);
-    //const [countItems, setCountItems] = useState(0);
+    const [countItems, setCountItems] = useState(0);
 
     
-/*     const handleCountItemsInCart = () => {
-        setCountItems(countItemsInCart());
-    return countItems;
-    } */
+    
+    
+    useEffect(() => {
+        const count = countItemsInCart();
+        setCountItems(count);
+    }, [countItemsInCart, countItems]);
+    
 
-/*     useEffect(() => {
-        setCountItems(countItemsInCart());
-    }, [countItems]); */
-
-    const handleCountItemsInCart = countItemsInCart();
+    //const handleCountItemsInCart = countItemsInCart();
 
     
 
@@ -31,7 +30,7 @@ const CartWidget = () => {
                 value={{size: '2.5rem' }}>
                 <Container className="d-flex">
                     <FaShoppingCart className="m-1" />
-                    <Badge bg="danger" className="m-2 p-2">{handleCountItemsInCart}</Badge>    
+                    <Badge bg="danger" className="m-2 p-2">{countItems}</Badge>    
                 </Container>
             </IconContext.Provider>
         : (null)}
