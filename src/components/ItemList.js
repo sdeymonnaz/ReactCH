e57958a1c93208efeedd595ab4225b749e72c5d8
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-11-11 23:56:36 -0300

    files unused

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,14 +1,8 @@
-//import { Container, Spinner } from "react-bootstrap";
 import "./ItemList.css";
-//import { promises } from "./promise";
 import Item from "./Item.js";
 
 
 const ItemList = ({ products }) => {
-  
-  
-
-
 
   return (
     <div className="container-fluid" id="listContainer">
