ff298e2fec515f5001796b50c090cc6b1958bd7b
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-11-16 22:55:13 -0300

    navbar fixed, icon added

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,13 +1,19 @@
 import {Navbar, Container, Nav, NavDropdown, NavItem} from 'react-bootstrap';
+import { IconContext } from "react-icons";
+import { FaBookOpen } from 'react-icons/fa';
 import { NavLink , Link} from "react-router-dom";
 import CartWidget from '../widgets/CartWidget.js';
 import './NavBar.css'
 
 const NavBar = () =>{
     return(
+
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand>
+                <Navbar.Brand className="d-flex">
+                    <IconContext.Provider
+                        value={{size: '2.5rem' }}><FaBookOpen className="m-2 navBarItem" />
+                    </IconContext.Provider>
                     <Link to="/" className="navBarItem" id="navBarHome">Apolloni Books</Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -15,15 +21,9 @@ const NavBar = () =>{
                 <Nav className="me-auto">
                     <NavLink exact to="/" activeClassName="selected" className="navBarItem">Home</NavLink>
                     <NavDropdown title="Category" id="basic-nav-dropdown">
-                        <NavDropdown.Item>
-                            <NavLink exact to="/category/novels" activeClassName="selected" className="navBarItem">Novels</NavLink>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>
-                            <NavLink exact to="/category/fiction" activeClassName="selected" className="navBarItem">Fiction</NavLink>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>
-                            <NavLink exact to="/category/biography" activeClassName="selected" className="navBarItem">Biography</NavLink>
-                        </NavDropdown.Item>
+                        <NavLink exact to="/category/novels" activeClassName="selected" className="navBarItem">Novels</NavLink>
+                        <NavLink exact to="/category/fiction" activeClassName="selected" className="navBarItem">Fiction</NavLink>
+                        <NavLink exact to="/category/biography" activeClassName="selected" className="navBarItem">Biography</NavLink>
                     </NavDropdown>
                 </Nav>
                 <NavItem className="d-flex justify-content-end">
